4b3002fb4e6ecf0e9653e78717220844143585fe
ramaniyer8195 <130081168+ramaniyer8195@users.noreply.github.com>
2023-07-26 22:45:48 +0530

    Adding the colors (#1)
    * Addind test colors
    
    * Added color values
    
    * updated package version
    
    * Added palette for color attributes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,25 @@
+// Color component exports
+export {
+  red,
+  pink,
+  purple,
+  deepPurple,
+  indigo,
+  blue,
+  lightBlue,
+  cyan,
+  teal,
+  green,
+  lightGreen,
+  lime,
+  yellow,
+  amber,
+  orange,
+  deepOrange,
+  brown,
+  grey,
+  blueGrey,
+} from "./Color";
+
+// Button component exports
 export { default as Button } from "./Button";
