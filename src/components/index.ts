be508c2f67ce5e3cc528e54ef579571ae5fa9871
Raman Iyer <ramaniyer8195@gmail.com>
2023-07-26 21:32:25 +0530

    Added color values

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,25 @@
+// Color component exports
+export {
+  red,
+  pink,
+  purple,
+  deepPurple,
+  indigo,
+  blue,
+  lightBlue,
+  cyan,
+  teal,
+  green,
+  lightGreen,
+  lime,
+  yellow,
+  amber,
+  orange,
+  deepOrange,
+  brown,
+  grey,
+  blueGrey,
+} from "./Color";
+
+// Button component exports
 export { default as Button } from "./Button";
-export { red, blue, green, pink, yellow } from "./Color";
